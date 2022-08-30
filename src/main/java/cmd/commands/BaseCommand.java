aa33095717d4ab76d43aebe256445c354ec6e612
MikeSchumacher <mike.schumacher@capgemini.com>
2022-08-30 10:32:49 +0200

    added command classes to baseCommand

--- src/main/java/cmd/commands/BaseCommand.java
+++ src/main/java/cmd/commands/BaseCommand.java
@@ -8,25 +8,25 @@ import picocli.CommandLine.Command;
 /**
  * Base Command Class
  * <p/>
- * This parent command class is meant to register other commands as subcommands.
- * This can be done using the subcommands parameter of the @Command annotation.
+ * This parent command class is meant to register other commands as subcommands. This can be done using the subcommands
+ * parameter of the @Command annotation.
  * <p/>
  * For a deeper understanding of how this is tied together please check the picocli documentation.
+ *
  * @see <a href="https://picocli.info/">picocli Documentation</a>
  */
-@Command(
-        name = "cmd",
-        description = "base command",
-        mixinStandardHelpOptions = true,
-        subcommands = {DirCommand.class, DelCommand.class, CopyCommand.class})
+@Command(name = "cmd", description = "base command", mixinStandardHelpOptions = true, subcommands = { DirCommand.class,
+DelCommand.class, CopyCommand.class, CDCommand.class, MkdirCommand.class, MoveCommand.class })
 public class BaseCommand implements Runnable {
-    public BaseCommand() {
-        /* intentionally empty */
-    }
+  public BaseCommand() {
 
-    @Override
-    public void run() {
-        /* intentionally empty */
-    }
+    /* intentionally empty */
+  }
+
+  @Override
+  public void run() {
+
+    /* intentionally empty */
+  }
 
 }
